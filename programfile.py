92f8baa9fb4cd19534c047e891c9681a75386518
JakubWilczek98 <jakubwilczek98@gmail.com>
2021-10-30 17:27:51 +0200

    Inertia matrix for dynamics

--- programfile.py
+++ programfile.py
@@ -51,6 +51,8 @@ for i in range(lelements):
 
 #Stiffness matrix
 
+#Statics
+
 def matrixk():
     k = np.array([[1, 0, -1, 0],
                   [0, 0, 0, 0],
@@ -68,6 +70,24 @@ def matrixkprim(DC, k, x):
 
 k1 = matrixk()
 
+#Dynamics
+
+def matrixm():
+    m = np.array([[2,0,1,0],
+                 [0,2,0,1],
+                 [1,0,2,0],
+                 [0,1,0,2]])
+    return m
+
+def matrixmprim(DC, m, x):
+    A = 3.14 * (10 ** (-4))
+    l = math.sqrt((x[3]-x[1])**2+(x[4]-x[2])**2)
+    ro = 7500
+    matrixmprim = ((ro*A*l)/6)*(np.matmul(np.matmul(DC.T, m), DC))
+    return matrixmprim
+
+m1 = matrixm()
+
 #Stiffness matrix verification
 
 def spr(x, ssin, ccos):
@@ -80,9 +100,12 @@ def spr(x, ssin, ccos):
                                [-ssin*ccos, -ssin*ssin, ssin*ccos, ssin*ssin]])
     return spr
 
-#Stiffness matrix after agregation
+#Inertia matrix after agregation
+#Z1 - inertia matrix - statics
+#M_ini - inertia matrix - dynamics
 
 Z1 = np.zeros((2*int(maxvalue), 2*int(maxvalue)))
+M_ini = np.zeros((2*int(maxvalue), 2*int(maxvalue)))
 
 for element in elements:
     Z = np.zeros((2*int(maxvalue), 2*int(maxvalue)))
@@ -116,6 +139,34 @@ for element in elements:
     Z[n][m] = M[3][1]
     Z1 = Z1 + Z
 
+    # Dynamics ---------------------------------
+
+    Z_P2 = np.zeros((2 * int(maxvalue), 2 * int(maxvalue)))
+    M_P2 = matrixmprim((DC(ccos(element), ssin(element))), m1, element)
+
+    Z_P2[m - 1][m - 1] = M_P2[0][0]
+    Z_P2[m - 1][m] = M_P2[1][0]
+    Z_P2[m][m - 1] = M_P2[0][1]
+    Z[m][m] = M_P2[1][1]
+
+    Z_P2[n - 1][n - 1] = M_P2[2][2]
+    Z_P2[n - 1][n] = M_P2[3][2]
+    Z_P2[n][n - 1] = M_P2[2][3]
+    Z_P2[n][n] = M_P2[3][3]
+
+    Z_P2[m - 1][n - 1] = M_P2[0][2]
+    Z_P2[m - 1][n] = M_P2[1][2]
+    Z_P2[m][n - 1] = M_P2[0][3]
+    Z_P2[m][n] = M_P2[1][3]
+
+    Z_P2[n - 1][m - 1] = M_P2[2][0]
+    Z_P2[n - 1][m] = M_P2[3][0]
+    Z_P2[n][m - 1] = M_P2[2][1]
+    Z_P2[n][m] = M_P2[3][1]
+    M_ini = M_ini + Z_P2
+
+
+print(M_ini)
 
 #Force vector - length have to be 2*number of points
 
@@ -132,7 +183,7 @@ F = np.loadtxt('force.txt')
 length_F = len(F)
 F = F.reshape(length_F, 1)
 
-#Dirichlet's boundary conditions
+#Dirichlet's boundary conditions from file
 
 
 P = np.loadtxt('xyz.txt', dtype='str')
@@ -141,7 +192,7 @@ i = 0
 L = []
 
 for i in range(len(P)):
-    print(P[i][0])
+    #print(P[i][0])
     if P[i][1] == 'n':
         L.append(2*int(P[i][0])-2)
         L.append(2*int(P[i][0])-1)
